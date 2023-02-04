f316d43a3caaa5a9f14ce644d7442534cc659114
Mariselvan <mariselvan.murugasan@zoomrx.com>
2023-02-04 12:34:57 +0530

    Test with HTML statement initial push

--- src/main/java/com/thoughtworks/furniturerental/Customer.java
+++ src/main/java/com/thoughtworks/furniturerental/Customer.java
@@ -24,23 +24,7 @@ public class Customer {
         int frequentRenterPoints = 0;
         String result = "Rental Record for " + getName() + "\n";
         for (Rental each : rentals) {
-            double thisAmount = 0;
-            //determine amounts for each line
-            switch (each.getFurniture().getPriceCode()) {
-                case Furniture.REGULAR:
-                    thisAmount += 200;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 150;
-                    break;
-                case Furniture.NEW_LAUNCH:
-                    thisAmount += each.getDaysRented() * 300;
-                    break;
-                case Furniture.CHILDREN:
-                    thisAmount += 150;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 150;
-                    break;
-            }
+            double thisAmount = calculateRentalAmount(each);
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two days new launch rental
@@ -59,6 +43,57 @@ public class Customer {
         result += "You earned " + frequentRenterPoints
                 + " frequent renter points";
         return result;
+    }
+
+    public String htmlStatement() {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        String result = "<h1>Rental Record for " + getName() + "</h1>";
+        for (Rental each : rentals) {
+            double thisAmount = calculateRentalAmount(each);
+
+            // add frequent renter points
+            frequentRenterPoints++;
+            // add bonus for a two days new launch rental
+            if ((each.getFurniture().getPriceCode() == Furniture.NEW_LAUNCH)
+                    &&
+                    each.getDaysRented() > 1) frequentRenterPoints++;
+
+            //show figures for this rental
+            result += "<div>" + each.getFurniture().getTitle() + "\t" +
+                    thisAmount + "</div>";
+
+            totalAmount += thisAmount;
+        }
+
+        //add footer lines result
+        result += "<div><b>Amount owed is " + totalAmount + "</b></div>";
+        result += "<div><b>You earned " + frequentRenterPoints
+                + " frequent renter points</b></div>";
+
+        return result;
+    }
+
+    public double calculateRentalAmount(Rental each) {
+        double thisAmount = 0;
+
+        switch (each.getFurniture().getPriceCode()) {
+            case Furniture.REGULAR:
+                thisAmount += 200;
+                if (each.getDaysRented() > 2)
+                    thisAmount += (each.getDaysRented() - 2) * 150;
+                break;
+            case Furniture.NEW_LAUNCH:
+                thisAmount += each.getDaysRented() * 300;
+                break;
+            case Furniture.CHILDREN:
+                thisAmount += 150;
+                if (each.getDaysRented() > 3)
+                    thisAmount += (each.getDaysRented() - 3) * 150;
+                break;
+        }
+
+        return thisAmount;
     }
 }
 
--- src/test/java/com/thoughtworks/furniturerental/CustomerTest.java
+++ src/test/java/com/thoughtworks/furniturerental/CustomerTest.java
@@ -6,11 +6,66 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CustomerTest {
     @Test
-    public void test() {
+    public void customerNameTest() {
+        Customer customer = new Customer("Mari");
 
-        int expected = 3;
-
-        assertEquals(expected, 1 + 2);
+        assertEquals("Mari", customer.getName());
     }
 
+    @Test
+    public void customerStatementTestForNewLaunch() {
+        Furniture furniture = new Furniture("Chair", Furniture.NEW_LAUNCH);
+        Rental rental = new Rental(furniture, 10);
+        Customer customer = new Customer("Mari");
+        customer.addRental(rental);
+
+        String statement = customer.statement();
+
+        assertEquals("Rental Record for Mari\n" +
+                "\tChair\t3000.0\n" +
+                "Amount owed is 3000.0\n" +
+                "You earned 2 frequent renter points", statement);
+    }
+
+    @Test
+    public void customerStatementTestForRegular() {
+        Furniture furniture = new Furniture("Chair", Furniture.REGULAR);
+        Rental rental = new Rental(furniture, 10);
+        Customer customer = new Customer("Mari");
+        customer.addRental(rental);
+
+        String statement = customer.statement();
+
+        assertEquals("Rental Record for Mari\n" +
+                "\tChair\t1400.0\n" +
+                "Amount owed is 1400.0\n" +
+                "You earned 1 frequent renter points", statement);
+    }
+
+    @Test
+    public void customerStatementTestForChildren() {
+        Furniture furniture = new Furniture("Chair", Furniture.CHILDREN);
+        Rental rental = new Rental(furniture, 10);
+        Customer customer = new Customer("Mari");
+        customer.addRental(rental);
+
+        String statement = customer.statement();
+
+        assertEquals("Rental Record for Mari\n" +
+                "\tChair\t1200.0\n" +
+                "Amount owed is 1200.0\n" +
+                "You earned 1 frequent renter points", statement);
+    }
+
+    @Test
+    public void customerHTMLStatementTestForNewLaunch() {
+        Furniture furniture = new Furniture("Chair", Furniture.NEW_LAUNCH);
+        Rental rental = new Rental(furniture, 10);
+        Customer customer = new Customer("Mari");
+        customer.addRental(rental);
+
+        String statement = customer.htmlStatement();
+
+        assertEquals("<h1>Rental Record for Mari</h1><div>Chair	3000.0</div><div><b>Amount owed is 3000.0</b></div><div><b>You earned 2 frequent renter points</b></div>", statement);
+    }
 }
